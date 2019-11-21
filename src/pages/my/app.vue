6b909269ea91846b59988863bebee6cae45440dc
gopher <gopherhehe@gmail.com>
2019-11-21 19:42:32 +0800

    app.vue -> App.vue app.js -> main.js

--- src/pages/my/app.vue
+++ src/pages/my/app.vue
@@ -9,7 +9,7 @@
 import hello from '@/components/hello.vue'
 // import hello from '../../components/hello.vue'
 export default {
-  // name: 'app',
+  name: 'App',
   components: {
     hello
   }
--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const titles = require('./title.js')
 const glob = require('glob')
 const pages = {}
 
-glob.sync('./src/pages/**/app.js').forEach(path => {
-  const chunk = path.split('./src/pages/')[1].split('/app.js')[0]
+glob.sync('./src/pages/**/main.js').forEach(path => {
+  const chunk = path.split('./src/pages/')[1].split('/main.js')[0]
   pages[chunk] = {
     entry: path,
     template: 'public/index.html',
